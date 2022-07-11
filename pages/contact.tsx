10d16fa94f09870e05111dd9f479fbe5541d5a12
RJGY <rjgy2yu@gmail.com>
2022-07-11 21:28:43 +1000

    added more pages

---
+++ pages/contact.tsx
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+import Layout from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import Date from '../components/date'
+
+export default function Work() {
+    return (
+        <Layout>
+            <Head>
+                <title>Work</title>
+            </Head>
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Work</h2>
+                <ul className={utilStyles.list}>
+                    <li className={utilStyles.listItem}>
+                        <Link href="/work/1">
+                            <a>
+                                <h3>
+                                    asdf
+                                </h3>
+                            </a>
+                        </Link>
+                        <br />
+                        <small className={utilStyles.lightText}>
+                            <Date dateString="2020-01-01" />
+                        </small>
+                    </li>
+                </ul>
+            </section>
+        </Layout>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -22,25 +22,6 @@ export default function Home({
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Welcome to my website!</p>
-        <p>
-          These are my projects.
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
       </section>
     </Layout>
   )
