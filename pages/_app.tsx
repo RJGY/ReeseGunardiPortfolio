2514c1c5ce57a4470c5e911bc948035f12de7074
RJGY <rjgy2yu@gmail.com>
2022-07-21 00:27:51 +1000

    removed styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import 'scss/main.scss'
+import 'styles/global.css'
 import { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,110 +1,20 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from 'components/layout'
-import icon from 'images/icon.png'
-import PropTypes from 'prop-types'
+import utilStyles from '../styles/utils.module.css'
 
-const Home = props => {
+export default function Home() {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <header id="header" style={props.timeout ? { display: 'none' } : {}}>
-        <div className="logo">
-          <span className="icon">
-            <img src={icon.src} alt="" height="100%" width="100%"/>
-          </span>
-        </div>
-        <div className="content">
-          <div className="inner">
-            <h1>LiteTech</h1>
-            <p>
-              Welcome to the LiteTech Minecraft Server
-            </p>
-          </div>
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <button
-                className="transparent-button"
-                onClick={() => {
-                  props.onOpenArticle('about')
-                }}
-              >
-                About
-              </button>
-            </li>
-            <li>
-              <button
-                className="transparent-button"
-                onClick={() => {
-                  props.onOpenArticle('image')
-                }}
-              >
-                Images
-              </button>
-            </li>
-            <li>
-              <button
-                className="transparent-button"
-                onClick={() => {
-                  props.onOpenArticle('members')
-                }}
-              >
-                Members
-              </button>
-            </li>
-            <li>
-              <button
-                className="transparent-button"
-                onClick={() => {
-                  props.onOpenArticle('youtube')
-                }}
-              >
-                Youtube
-              </button>
-            </li>
-            <li>
-              <button
-                className="transparent-button"
-                onClick={() => {
-                  props.onOpenArticle('discord')
-                }}
-              >
-                Discord
-              </button>
-            </li>
-            <li>
-              <a href="https://panel.litete.ch/" target="_blank"  rel="noreferrer">
-                <button
-                  className="transparent-button"
-                  type="submit"
-                >
-                  Panel
-                </button>
-              </a>
-            </li>
-            <li>
-              <a href="https://archives.litete.ch/" target="_blank"  rel="noreferrer">
-                <button
-                  className="transparent-button"
-                  type="submit"
-                >
-                  Archive
-                </button>
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this in{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
     </Layout>
   )
-}
-
-export default Home
-
-Home.propTypes = {
-  onOpenArticle: PropTypes.func,
-  timeout: PropTypes.bool,
 }
