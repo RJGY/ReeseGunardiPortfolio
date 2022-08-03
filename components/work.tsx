70c342996d2b72230217c317ed1239a207391216
RJGY <rjgy2yu@gmail.com>
2022-08-03 23:39:57 +1000

    formatting of code

--- components/work.tsx
+++ components/work.tsx
@@ -4,7 +4,7 @@ export default function Work() {
     return (
         <>
             <div className="normal-gap">
-                <p className="fs-1">Work & Education</p>
+                <p className="fs-1">Experience & Education</p>
                 <Tabs
                     defaultActiveKey="work"
                     className="mb-3"
@@ -14,27 +14,39 @@ export default function Work() {
                         <Tab.Container defaultActiveKey="first">
                             <Row>
                                 <Col sm={3}>
-                                <Nav variant="pills" className="flex-column">
-                                    <Nav.Item>
-                                        <Nav.Link eventKey="first">ASX & Ampion</Nav.Link>
-                                    </Nav.Item>
-                                    <Nav.Item>
-                                        <Nav.Link eventKey="second">Sailing</Nav.Link>
-                                    </Nav.Item>
-                                </Nav>
+                                    <Nav variant="pills" className="flex-column">
+                                        <Nav.Item>
+                                            <Nav.Link eventKey="first">
+                                                <p>ASX & Ampion</p>
+                                            </Nav.Link>
+                                        </Nav.Item>
+                                        <Nav.Item>
+                                            <Nav.Link eventKey="second">
+                                                <p>Global Imaging</p>
+                                            </Nav.Link>
+                                        </Nav.Item>
+                                        <Nav.Item>
+                                            <Nav.Link eventKey="third">
+                                                <p>Foxtel</p>
+                                            </Nav.Link>
+                                        </Nav.Item>
+                                    </Nav>
                                 </Col>
                                 <Col sm={9}>
-                                <Tab.Content>
-                                    <Tab.Pane eventKey="first">
-                                        one
-                                    </Tab.Pane>
-                                    <Tab.Pane eventKey="second">
-                                        two
-                                    </Tab.Pane>
-                                    <Tab.Pane eventKey="third">
-                                        three
-                                    </Tab.Pane>
-                                </Tab.Content>
+                                    <Tab.Content>
+                                        <Tab.Pane eventKey="first">
+                                            <p>Test Automation Engineer & Junior Developer</p>
+                                            <p></p>
+                                        </Tab.Pane>
+                                        <Tab.Pane eventKey="second">
+                                            <p>Admin</p>
+                                            <p></p>
+                                        </Tab.Pane>
+                                        <Tab.Pane eventKey="third">
+                                            <p>Admin</p>
+                                            <p></p>
+                                        </Tab.Pane>
+                                    </Tab.Content>
                                 </Col>
                             </Row>
                         </Tab.Container>
@@ -45,13 +57,19 @@ export default function Work() {
                                 <Col sm={3}>
                                 <Nav variant="pills" className="flex-column">
                                     <Nav.Item>
-                                        <Nav.Link eventKey="first">Bachelors of Information Technology</Nav.Link>
+                                        <Nav.Link eventKey="first">
+                                            Bachelors of Information Technology
+                                        </Nav.Link>
                                     </Nav.Item>
                                     <Nav.Item>
-                                        <Nav.Link eventKey="second">St Leonards Tafe</Nav.Link>
+                                        <Nav.Link eventKey="second">
+                                            St Leonards Tafe
+                                        </Nav.Link>
                                     </Nav.Item>
                                     <Nav.Item>
-                                        <Nav.Link eventKey="third">Ryde Secondary College</Nav.Link>
+                                        <Nav.Link eventKey="third">
+                                            Ryde Secondary College
+                                        </Nav.Link>
                                     </Nav.Item>
                                 </Nav>
                                 </Col>
