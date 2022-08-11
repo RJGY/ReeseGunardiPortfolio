80139ab26c911678fafacf755f0c90fcce0e259d
RJGY <rjgy2yu@gmail.com>
2022-08-11 11:50:05 +1000

    fixed header

--- README.md
+++ README.md
@@ -10,22 +10,11 @@ Github actions
 Stolen from timz.dev
 
 ## TODO
- * BETTER FONTS IMPORTANT, NEED THINNER FONT
- * Work & education list like examples
- * Gradient background. Blue to black
- * Text colors in some shade in light blue/white/cyan/green
- * Change scrollbar
- * Better profile picture cause your one sucks 
- * On hover animations for cards
- * Add tyms contact form to contact
- * add footer links to like linkedin and github
- * add image to about me
- * add notable work section to about me
- * work & education section is kinda gimped so i need to add perma space :deadpan:
- * fix colors with work & education
- * add font for navbar
- * fix blue background work and education
- * make size of experience and education constant
- * Add oauth2 to contact so that we dont get spammed
- * remove space in contactme
- 
+* Change scrollbar
+* Better profile picture cause your one sucks 
+* On hover animations for cards
+* add notable work section to about me
+* add font for navbar
+* make size of experience and education constant
+* Add oauth2 to contact so that we dont get spammed
+* Mobile responsive when???
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,25 +1,23 @@
-import { Row, Col } from 'react-bootstrap';
+import linkedin from 'public/linkedin logo.png'
+import github from 'public/github logo.png'
+
+import Image from 'next/image'
 
 export default function Footer() {
     return (
         <>  
             <div className="footer">
-                <Row>
-                    <Col sm={1}>
-                        
-                    </Col>
-                    <Col sm={10}>
-                        <p></p>
-                    </Col>       
-                    <Col sm={1}>
-                        
-                            <div className="text-end">
-                                <p className="fs-4 footer-text">rjgy2yu@gmail.com</p>
-                            </div>
-                        
-                    </Col>
-                </Row>
+                <div className="m-1">
+                    <a href="https://github.com/RJGY" target="_blank">
+                        <Image src={github} className="p-1" height="60" width="60" layout="intrinsic" alt="Logo" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/reese-gunardi-485ba6184/" target="_blank">
+                        <Image src={linkedin} className="p-1" height="60" width="60" layout="intrinsic" alt="Logo" />
+                    </a>
+                    <p className="fs-5 white m-1">rjgy2yu@gmail.com</p>
+                </div>
             </div>
+            
         </>
     )
 }
