25920d5f12fbe02ef589ca7950a83ccdcf5aebc4
Reese Gunardi <rjgy2yu@gmail.com>
2024-12-08 00:44:52 +1100

    update portfolio

--- components/about.tsx
+++ components/about.tsx
@@ -1,6 +1,6 @@
 import { Col, Row } from 'react-bootstrap';
 import Image from 'next/image'
-import profile from 'images/profile_final.jpg';
+import profile from 'images/profile_image.jpg';
 
 export default function About() {
     return (
@@ -11,7 +11,7 @@ export default function About() {
                     <Col sm={8}>
                         <p className="fs-1">Who am I?</p>
                         <p className="fs-5">
-                            Hey, my name is Reese Gunardi and I am a fullstack developer currently working at Objective Corporation and I build 
+                            Hey, my name is Reese Gunardi and I am a full-stack developer currently working at Objective Corporation and I build 
                             just about anything and everything. I started coding as a teenager aged 14 when I was interested in modding video games 
                             and creating my own games. Since then, I have discovered my passion for programming and creating software, and have completed my 
                             Bachelors of Information Technology (Co-op) at Central Queensland Univeristy and a Certificate IV in Interactive Games 
